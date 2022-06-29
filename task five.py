a9a348be019e1d4c8761eefb59daffea016c3f87
vovkodav21 <vishtalyurii@gmail.com>
2022-06-29 18:58:33 +0300

    task 4

--- task five.py
+++ task five.py
@@ -5,10 +5,13 @@ for el in range(0,len(astring)):
     else:
         continue
 #можна print(*astring[::2])
+print(len([i for i in astring if i.isdigit()]))
+print(len([i for i in astring if i.isalpha()]))
 print(*astring[1::2])
 print(astring[2]) #a. Виведіть третій символ цього рядка.
 print(astring[-2]) # b. виведіть передостанній символ цього рядка.
 print(astring[0:5]) #с. виведіть перші п'ять символів цього рядка.
 print(astring[:-2]) #d. виведіть весь рядок, крім двох останніх символів.
 print(astring[::-1])
+
 
